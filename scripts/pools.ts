9d2846a0c2e9e48b215e23fe1a69f2c2108fe518
crypzoh <crypzoh@protonmail.com>
2022-04-21 00:36:25 +0100

    Add some debug scripts

--- scripts/pools.ts
+++ scripts/pools.ts
@@ -3,46 +3,64 @@ import '@nomiclabs/hardhat-ethers'
 import { Logger } from 'tslog'
 import config from './config/config'
 import { ComplexRewarderTime, MiniChefV2 } from '../dist/types'
-import { parseEther } from 'ethers/lib/utils'
+import { BigNumber } from 'ethers'
 
 const logger: Logger = new Logger()
 
-task('set-emission')
-    .setAction(async (args, hre) => {
-        const instance = await hre.ethers.getContractAt("MiniChefV2", config.miniChef) as MiniChefV2;
+task('set-emission', 'Set Emission').setAction(async (args, hre) => {
+  const instance = (await hre.ethers.getContractAt('MiniChefV2', config.miniChef)) as MiniChefV2
 
-        const res = await instance.setDiffusionPerSecond(parseEther(config.rewardsPerSecond));
+  const res = await instance.setDiffusionPerSecond(config.rewardsPerSecond)
 
-        logger.info(res);
-    });
+  logger.info(res)
+})
 
-task('set-emission-complexreward')
-    .setAction(async (args, hre) => {
-        const instance = await hre.ethers.getContractAt("ComplexRewarderTime", config.complexRewarderTime) as ComplexRewarderTime;
+task('set-pool-share', 'Set Pool Share').setAction(async (args, hre) => {
+  const instance = (await hre.ethers.getContractAt('MiniChefV2', config.miniChef)) as MiniChefV2
 
-        const res = await instance.setRewardPerSecond(parseEther(config.rewardsPerSecond));
+  // const length = await instance.set(BigNumber.from(0),BigNumber.from(100),)
+  logger.info('Length', length)
+  1
+  const res = await instance.poolInfo(BigNumber.from(0))
 
-        logger.info(res);
-    });
+  logger.info('PoolInfo', res)
+})
+
+task('log-emission', 'Log Emission').setAction(async (args, hre) => {
+  const instance = (await hre.ethers.getContractAt('MiniChefV2', config.miniChef)) as MiniChefV2
+
+  const length = await instance.poolLength()
+  logger.info('Length', length)
+
+  const res = await instance.poolInfo(BigNumber.from(0))
+  logger.info('PoolInfo', res)
+
+  const globalPerSecond = await instance.diffusionPerSecond()
+
+  logger.info(globalPerSecond)
+})
 
 task('add-pool', 'Adds Pool')
-    .addParam('allocpoint', 'Pool allocation points')
-    .addParam('lptoken', 'LP token for the pool')
-    .setAction(async (args, hre) => {
-        const instance = await hre.ethers.getContractAt("MiniChefV2", config.miniChef) as MiniChefV2;
+  .addParam('allocpoint', 'Pool allocation points')
+  .addParam('lptoken', 'LP token for the pool')
+  .setAction(async (args, hre) => {
+    const instance = (await hre.ethers.getContractAt('MiniChefV2', config.miniChef)) as MiniChefV2
 
-        const res = await instance.add(args.allocpoint, args.lptoken, config.complexRewarderTime);
+    const res = await instance.add(args.allocpoint, args.lptoken, config.complexRewarderTime)
 
-        logger.info(res);
-    });
+    logger.info(res)
+  })
 
 task('add-rewarder', 'Adds Pool to complexRewarder')
-    .addParam('allocpoint', 'Pool allocation points')
-    .addParam('pid', 'Pool ID for the pool')
-    .setAction(async (args, hre) => {
-        const instance = await hre.ethers.getContractAt("ComplexRewarderTime", config.complexRewarderTime) as ComplexRewarderTime;
+  .addParam('allocpoint', 'Pool allocation points')
+  .addParam('pid', 'Pool ID for the pool')
+  .setAction(async (args, hre) => {
+    const instance = (await hre.ethers.getContractAt(
+      'ComplexRewarderTime',
+      config.complexRewarderTime
+    )) as ComplexRewarderTime
 
-        const res = await instance.add(args.allocpoint, args.pid);
+    const res = await instance.add(args.allocpoint, args.pid)
 
-        logger.info(res);
-    });
+    logger.info(res)
+  })
