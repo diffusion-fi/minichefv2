126bcd1dd40cfdb2010c041e0b87dbaf6df11739
velvetgopher <velvetgopher@protonmail.com>
2022-10-17 15:33:31 -0400

    flattened files

---
+++ flatt_ComplexRewarderTime.sol
@@ -0,0 +1,413 @@
+// Sources flattened with hardhat v2.8.4 https://hardhat.org
+
+// File @boringcrypto/boring-solidity/contracts/interfaces/IERC20.sol@v1.0.4
+
+// SPDX-License-Identifier: MIT
+pragma solidity 0.6.12;
+
+interface IERC20 {
+    function totalSupply() external view returns (uint256);
+    function balanceOf(address account) external view returns (uint256);
+    function allowance(address owner, address spender) external view returns (uint256);
+    function approve(address spender, uint256 amount) external returns (bool);
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    // EIP 2612
+    function permit(address owner, address spender, uint256 value, uint256 deadline, uint8 v, bytes32 r, bytes32 s) external;
+}
+
+
+// File @boringcrypto/boring-solidity/contracts/libraries/BoringERC20.sol@v1.0.4
+
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.6.12;
+library BoringERC20 {
+    function safeSymbol(IERC20 token) internal view returns(string memory) {
+        (bool success, bytes memory data) = address(token).staticcall(abi.encodeWithSelector(0x95d89b41));
+        return success && data.length > 0 ? abi.decode(data, (string)) : "???";
+    }
+
+    function safeName(IERC20 token) internal view returns(string memory) {
+        (bool success, bytes memory data) = address(token).staticcall(abi.encodeWithSelector(0x06fdde03));
+        return success && data.length > 0 ? abi.decode(data, (string)) : "???";
+    }
+
+    function safeDecimals(IERC20 token) internal view returns (uint8) {
+        (bool success, bytes memory data) = address(token).staticcall(abi.encodeWithSelector(0x313ce567));
+        return success && data.length == 32 ? abi.decode(data, (uint8)) : 18;
+    }
+
+    function safeTransfer(IERC20 token, address to, uint256 amount) internal {
+        (bool success, bytes memory data) = address(token).call(abi.encodeWithSelector(0xa9059cbb, to, amount));
+        require(success && (data.length == 0 || abi.decode(data, (bool))), "BoringERC20: Transfer failed");
+    }
+
+    function safeTransferFrom(IERC20 token, address from, address to, uint256 amount) internal {
+        (bool success, bytes memory data) = address(token).call(abi.encodeWithSelector(0x23b872dd, from, to, amount));
+        require(success && (data.length == 0 || abi.decode(data, (bool))), "BoringERC20: TransferFrom failed");
+    }
+}
+
+
+// File contracts/interfaces/IRewarder.sol
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.6.12;
+
+interface IRewarder {
+    using BoringERC20 for IERC20;
+    function onDiffusionReward(uint256 pid, address user, address recipient, uint256 diffusionAmount, uint256 newLpAmount) external;
+    function pendingTokens(uint256 pid, address user, uint256 diffusionAmount) external view returns (IERC20[] memory, uint256[] memory);
+}
+
+
+// File contracts/interfaces/IMasterChef.sol
+
+// SPDX-License-Identifier: MIT
+pragma solidity 0.6.12;
+pragma experimental ABIEncoderV2;
+
+interface IMasterChef {
+    using BoringERC20 for IERC20;
+    struct UserInfo {
+        uint256 amount;     // How many LP tokens the user has provided.
+        uint256 rewardDebt; // Reward debt. See explanation below.
+    }
+
+    struct PoolInfo {
+        IERC20 lpToken;           // Address of LP token contract.
+        uint256 allocPoint;       // How many allocation points assigned to this pool. SUSHI to distribute per block.
+        uint256 lastRewardBlock;  // Last block number that SUSHI distribution occurs.
+        uint256 accSushiPerShare; // Accumulated SUSHI per share, times 1e12. See below.
+    }
+
+    function poolInfo(uint256 pid) external view returns (IMasterChef.PoolInfo memory);
+    function totalAllocPoint() external view returns (uint256);
+    function deposit(uint256 _pid, uint256 _amount) external;
+    function lpToken(uint256) external view returns (IERC20);
+}
+
+
+// File @boringcrypto/boring-solidity/contracts/libraries/BoringMath.sol@v1.0.4
+
+// SPDX-License-Identifier: MIT
+pragma solidity 0.6.12;
+// a library for performing overflow-safe math, updated with awesomeness from of DappHub (https://github.com/dapphub/ds-math)
+library BoringMath {
+    function add(uint256 a, uint256 b) internal pure returns (uint256 c) {require((c = a + b) >= b, "BoringMath: Add Overflow");}
+    function sub(uint256 a, uint256 b) internal pure returns (uint256 c) {require((c = a - b) <= a, "BoringMath: Underflow");}
+    function mul(uint256 a, uint256 b) internal pure returns (uint256 c) {require(b == 0 || (c = a * b)/b == a, "BoringMath: Mul Overflow");}
+    function to128(uint256 a) internal pure returns (uint128 c) {
+        require(a <= uint128(-1), "BoringMath: uint128 Overflow");
+        c = uint128(a);
+    }
+    function to64(uint256 a) internal pure returns (uint64 c) {
+        require(a <= uint64(-1), "BoringMath: uint64 Overflow");
+        c = uint64(a);
+    }
+    function to32(uint256 a) internal pure returns (uint32 c) {
+        require(a <= uint32(-1), "BoringMath: uint32 Overflow");
+        c = uint32(a);
+    }
+}
+
+library BoringMath128 {
+    function add(uint128 a, uint128 b) internal pure returns (uint128 c) {require((c = a + b) >= b, "BoringMath: Add Overflow");}
+    function sub(uint128 a, uint128 b) internal pure returns (uint128 c) {require((c = a - b) <= a, "BoringMath: Underflow");}
+}
+
+library BoringMath64 {
+    function add(uint64 a, uint64 b) internal pure returns (uint64 c) {require((c = a + b) >= b, "BoringMath: Add Overflow");}
+    function sub(uint64 a, uint64 b) internal pure returns (uint64 c) {require((c = a - b) <= a, "BoringMath: Underflow");}
+}
+
+library BoringMath32 {
+    function add(uint32 a, uint32 b) internal pure returns (uint32 c) {require((c = a + b) >= b, "BoringMath: Add Overflow");}
+    function sub(uint32 a, uint32 b) internal pure returns (uint32 c) {require((c = a - b) <= a, "BoringMath: Underflow");}
+}
+
+
+// File @boringcrypto/boring-solidity/contracts/BoringOwnable.sol@v1.0.4
+
+// SPDX-License-Identifier: MIT
+// Audit on 5-Jan-2021 by Keno and BoringCrypto
+
+// P1 - P3: OK
+pragma solidity 0.6.12;
+
+// Source: https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol + Claimable.sol
+// Edited by BoringCrypto
+
+// T1 - T4: OK
+contract BoringOwnableData {
+    // V1 - V5: OK
+    address public owner;
+    // V1 - V5: OK
+    address public pendingOwner;
+}
+
+// T1 - T4: OK
+contract BoringOwnable is BoringOwnableData {
+    // E1: OK
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    constructor () public {
+        owner = msg.sender;
+        emit OwnershipTransferred(address(0), msg.sender);
+    }
+
+    // F1 - F9: OK
+    // C1 - C21: OK
+    function transferOwnership(address newOwner, bool direct, bool renounce) public onlyOwner {
+        if (direct) {
+            // Checks
+            require(newOwner != address(0) || renounce, "Ownable: zero address");
+
+            // Effects
+            emit OwnershipTransferred(owner, newOwner);
+            owner = newOwner;
+            pendingOwner = address(0);
+        } else {
+            // Effects
+            pendingOwner = newOwner;
+        }
+    }
+
+    // F1 - F9: OK
+    // C1 - C21: OK
+    function claimOwnership() public {
+        address _pendingOwner = pendingOwner;
+        
+        // Checks
+        require(msg.sender == _pendingOwner, "Ownable: caller != pending owner");
+
+        // Effects
+        emit OwnershipTransferred(owner, _pendingOwner);
+        owner = _pendingOwner;
+        pendingOwner = address(0);
+    }
+
+    // M1 - M5: OK
+    // C1 - C21: OK
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Ownable: caller is not the owner");
+        _;
+    }
+}
+
+
+// File contracts/ComplexRewarderTime.sol
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.6.12;
+pragma experimental ABIEncoderV2;
+
+
+
+
+
+/// @author @0xKeno
+contract ComplexRewarderTime is IRewarder,  BoringOwnable{
+    using BoringMath for uint256;
+    using BoringMath128 for uint128;
+    using BoringERC20 for IERC20;
+
+    IERC20 private immutable rewardToken;
+
+    /// @notice Info of each MCV2 user.
+    /// `amount` LP token amount the user has provided.
+    /// `rewardDebt` The amount of DIFFUSION entitled to the user.
+    struct UserInfo {
+        uint256 amount;
+        uint256 rewardDebt;
+        uint256 unpaidRewards;
+    }
+
+    /// @notice Info of each MCV2 pool.
+    /// `allocPoint` The amount of allocation points assigned to the pool.
+    /// Also known as the amount of DIFFUSION to distribute per block.
+    struct PoolInfo {
+        uint128 accDiffusionPerShare;
+        uint64 lastRewardTime;
+        uint64 allocPoint;
+    }
+
+    /// @notice Info of each pool.
+    mapping (uint256 => PoolInfo) public poolInfo;
+
+    uint256[] public poolIds;
+
+    /// @notice Info of each user that stakes LP tokens.
+    mapping (uint256 => mapping (address => UserInfo)) public userInfo;
+    /// @dev Total allocation points. Must be the sum of all allocation points in all pools.
+    uint256 public totalAllocPoint;
+
+    uint256 public rewardPerSecond;
+    uint256 private constant ACC_TOKEN_PRECISION = 1e12;
+
+    address private immutable MASTERCHEF_V2;
+
+    uint256 internal unlocked;
+    modifier lock() {
+        require(unlocked == 1, "LOCKED");
+        unlocked = 2;
+        _;
+        unlocked = 1;
+    }
+
+    event LogOnReward(address indexed user, uint256 indexed pid, uint256 amount, address indexed to);
+    event LogPoolAddition(uint256 indexed pid, uint256 allocPoint);
+    event LogSetPool(uint256 indexed pid, uint256 allocPoint);
+    event LogUpdatePool(uint256 indexed pid, uint64 lastRewardTime, uint256 lpSupply, uint256 accDiffusionPerShare);
+    event LogRewardPerSecond(uint256 rewardPerSecond);
+    event LogInit();
+
+    constructor (IERC20 _rewardToken, uint256 _rewardPerSecond, address _MASTERCHEF_V2) public {
+        rewardToken = _rewardToken;
+        rewardPerSecond = _rewardPerSecond;
+        MASTERCHEF_V2 = _MASTERCHEF_V2;
+        unlocked = 1;
+    }
+
+
+    function onDiffusionReward (uint256 pid, address _user, address to, uint256, uint256 lpToken) onlyMCV2 lock override external {
+        PoolInfo memory pool = updatePool(pid);
+        UserInfo storage user = userInfo[pid][_user];
+        uint256 pending;
+        if (user.amount > 0) {
+            pending =
+                (user.amount.mul(pool.accDiffusionPerShare) / ACC_TOKEN_PRECISION).sub(
+                    user.rewardDebt
+                ).add(user.unpaidRewards);
+            uint256 balance = rewardToken.balanceOf(address(this));
+            if (pending > balance) {
+                rewardToken.safeTransfer(to, balance);
+                user.unpaidRewards = pending - balance;
+            } else {
+                rewardToken.safeTransfer(to, pending);
+                user.unpaidRewards = 0;
+            }
+        }
+        user.amount = lpToken;
+        user.rewardDebt = lpToken.mul(pool.accDiffusionPerShare) / ACC_TOKEN_PRECISION;
+        emit LogOnReward(_user, pid, pending - user.unpaidRewards, to);
+    }
+
+    function pendingTokens(uint256 pid, address user, uint256) override external view returns (IERC20[] memory rewardTokens, uint256[] memory rewardAmounts) {
+        IERC20[] memory _rewardTokens = new IERC20[](1);
+        _rewardTokens[0] = (rewardToken);
+        uint256[] memory _rewardAmounts = new uint256[](1);
+        _rewardAmounts[0] = pendingToken(pid, user);
+        return (_rewardTokens, _rewardAmounts);
+    }
+
+    /// @notice Sets the diffusion per second to be distributed. Can only be called by the owner.
+    /// @param _rewardPerSecond The amount of Diffusion to be distributed per second.
+    function setRewardPerSecond(uint256 _rewardPerSecond) public onlyOwner {
+        rewardPerSecond = _rewardPerSecond;
+        emit LogRewardPerSecond(_rewardPerSecond);
+    }
+
+    modifier onlyMCV2 {
+        require(
+            msg.sender == MASTERCHEF_V2,
+            "Only MCV2 can call this function."
+        );
+        _;
+    }
+
+    /// @notice Returns the number of MCV2 pools.
+    function poolLength() public view returns (uint256 pools) {
+        pools = poolIds.length;
+    }
+
+    /// @notice Add a new LP to the pool. Can only be called by the owner.
+    /// DO NOT add the same LP token more than once. Rewards will be messed up if you do.
+    /// @param allocPoint AP of the new pool.
+    /// @param _pid Pid on MCV2
+    function add(uint256 allocPoint, uint256 _pid) public onlyOwner {
+        require(poolInfo[_pid].lastRewardTime == 0, "Pool already exists");
+        uint256 lastRewardTime = block.timestamp;
+        totalAllocPoint = totalAllocPoint.add(allocPoint);
+
+        poolInfo[_pid] = PoolInfo({
+            allocPoint: allocPoint.to64(),
+            lastRewardTime: lastRewardTime.to64(),
+            accDiffusionPerShare: 0
+        });
+        poolIds.push(_pid);
+        emit LogPoolAddition(_pid, allocPoint);
+    }
+
+    /// @notice Update the given pool's DIFFUSION allocation point and `IRewarder` contract. Can only be called by the owner.
+    /// @param _pid The index of the pool. See `poolInfo`.
+    /// @param _allocPoint New AP of the pool.
+    function set(uint256 _pid, uint256 _allocPoint) public onlyOwner {
+        totalAllocPoint = totalAllocPoint.sub(poolInfo[_pid].allocPoint).add(_allocPoint);
+        poolInfo[_pid].allocPoint = _allocPoint.to64();
+        emit LogSetPool(_pid, _allocPoint);
+    }
+
+    /// @notice Allows owner to reclaim/withdraw any tokens (including reward tokens) held by this contract
+    /// @param token Token to reclaim, use 0x00 for Ethereum
+    /// @param amount Amount of tokens to reclaim
+    /// @param to Receiver of the tokens, first of his name, rightful heir to the lost tokens,
+    /// reightful owner of the extra tokens, and ether, protector of mistaken transfers, mother of token reclaimers,
+    /// the Khaleesi of the Great Token Sea, the Unburnt, the Breaker of blockchains.
+    function reclaimTokens(address token, uint256 amount, address payable to) public onlyOwner {
+        if (token == address(0)) {
+            to.transfer(amount);
+        } else {
+            IERC20(token).safeTransfer(to, amount);
+        }
+    }
+
+    /// @notice View function to see pending Token
+    /// @param _pid The index of the pool. See `poolInfo`.
+    /// @param _user Address of user.
+    /// @return pending DIFFUSION reward for a given user.
+    function pendingToken(uint256 _pid, address _user) public view returns (uint256 pending) {
+        PoolInfo memory pool = poolInfo[_pid];
+        UserInfo storage user = userInfo[_pid][_user];
+        uint256 accDiffusionPerShare = pool.accDiffusionPerShare;
+        uint256 lpSupply = IMasterChef(MASTERCHEF_V2).lpToken(_pid).balanceOf(MASTERCHEF_V2);
+        if (block.timestamp > pool.lastRewardTime && lpSupply != 0) {
+            uint256 time = block.timestamp.sub(pool.lastRewardTime);
+            uint256 diffusionReward = time.mul(rewardPerSecond).mul(pool.allocPoint) / totalAllocPoint;
+            accDiffusionPerShare = accDiffusionPerShare.add(diffusionReward.mul(ACC_TOKEN_PRECISION) / lpSupply);
+        }
+        pending = (user.amount.mul(accDiffusionPerShare) / ACC_TOKEN_PRECISION).sub(user.rewardDebt).add(user.unpaidRewards);
+    }
+
+    /// @notice Update reward variables for all pools. Be careful of gas spending!
+    /// @param pids Pool IDs of all to be updated. Make sure to update all active pools.
+    function massUpdatePools(uint256[] calldata pids) external {
+        uint256 len = pids.length;
+        for (uint256 i = 0; i < len; ++i) {
+            updatePool(pids[i]);
+        }
+    }
+
+    /// @notice Update reward variables of the given pool.
+    /// @param pid The index of the pool. See `poolInfo`.
+    /// @return pool Returns the pool that was updated.
+    function updatePool(uint256 pid) public returns (PoolInfo memory pool) {
+        pool = poolInfo[pid];
+        if (block.timestamp > pool.lastRewardTime) {
+            uint256 lpSupply = IMasterChef(MASTERCHEF_V2).lpToken(pid).balanceOf(MASTERCHEF_V2);
+
+            if (lpSupply > 0) {
+                uint256 time = block.timestamp.sub(pool.lastRewardTime);
+                uint256 diffusionReward = time.mul(rewardPerSecond).mul(pool.allocPoint) / totalAllocPoint;
+                pool.accDiffusionPerShare = pool.accDiffusionPerShare.add((diffusionReward.mul(ACC_TOKEN_PRECISION) / lpSupply).to128());
+            }
+            pool.lastRewardTime = block.timestamp.to64();
+            poolInfo[pid] = pool;
+            emit LogUpdatePool(pid, pool.lastRewardTime, lpSupply, pool.accDiffusionPerShare);
+        }
+    }
+
+}
