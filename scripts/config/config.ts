11107d9a2ee6844175e18839862924165dbe5a6b
velvetgopher <velvetgopher@protonmail.com>
2022-10-17 15:24:28 -0400

    add addresses to config

--- scripts/config/config.ts
+++ scripts/config/config.ts
@@ -1,11 +1,17 @@
+
 export default {
     rewardToken: "0x3f75ceabCDfed1aCa03257Dc6Bdc0408E2b4b026",
-    secondaryRewardToken: "",
+    secondaryRewardToken: "0x80b5a32E4F032B2a058b4F29EC95EEfEEB87aDcd",
     leftHandSideToken: "",
     rightHandSideToken:"",
     miniChef: "0x067eC87844fBD73eDa4a1059F30039584586e09d",
     xDiff: "0x75aeE82a16BD1fB98b11879af93AB7CE055f66Da",
-    complexRewarderTime: "0x0000000000000000000000000000000000000000",
+    complexRewarderTime: "0x91c483FDe16Ef79c38918a305F363B329156d1db", // wevmos
+    // complexRewarderTime: "0x2EBbC8705bccF572c2d267Ba1B9BF8d2C3aC8934",
+    // complexRewarderTime: "0x9a9e4bbA48E0cd7025A7cC9F5C3e8306b9fD66c1",
     diffusion: "0x3f75ceabCDfed1aCa03257Dc6Bdc0408E2b4b026",
-    rewardsPerSecond: "0.73"
+    weth9: "0xD4949664cD82660AaE99bEdc034a0deA8A0bd517",
+    newOwner: "0x697FEe65ECA6627AA7776bb5769B4445F11f8E99",
+    rewardsPerSecond: "0",
+    secondaryRewardsPerSecond: "155737",// no parsing
 }
